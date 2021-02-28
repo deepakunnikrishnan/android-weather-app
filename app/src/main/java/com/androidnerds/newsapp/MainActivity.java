614dbbf50b5f614df8bd66298e13ca6010b723b9
Deepak Unnikrishnan <deepak.unnikrishnan@ondotsystems.com>
2021-02-27 20:46:17 -0500

    Sample implementation of the Fragment in the app.

--- app/src/main/java/com/androidnerds/newsapp/MainActivity.java
+++ app/src/main/java/com/androidnerds/newsapp/MainActivity.java
@@ -1,14 +1,10 @@
 package com.androidnerds.newsapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.androidnerds.cardcarousel.CarouselView;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.androidnerds.weatherview.presentation.WeatherViewFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Card> cardList = new ArrayList<>();
-        cardList.add(new Card("1"));
-        cardList.add(new Card("2"));
-        cardList.add(new Card("3"));
-        cardList.add(new Card("4"));
-        cardList.add(new Card("5"));
-        cardList.add(new Card("6"));
-        cardList.add(new Card("7"));
-        cardList.add(new Card("8"));
-        CarouseViewAdapter adapter = new CarouseViewAdapter();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, WeatherViewFragment.newInstance())
+        .commitNow();
 
-        TextView textView = findViewById(R.id.textView);
-        CarouselView carouselView = findViewById(R.id.carouselView);
-        carouselView.setItemSelectedListener(position -> textView.setText("Position: "+(position+1)));
-
-        carouselView.setAdapter(adapter);
-        adapter.submitList(cardList);
     }
 }
